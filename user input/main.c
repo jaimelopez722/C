64ffd637e8af8e8e4c83986c01cca8409cf89c49
Jaime Lopez <afjaimelopez@gmail.com>
2026-04-22 20:55:14 -0700

    Adding user input

--- user input/main.c
+++ user input/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h> //preprocessor directive
+#include <string.h>
 
 int main(){
 
@@ -9,5 +10,24 @@ int main(){
 
     printf("Enter your age: ");
     scanf("%d", &age);
+
+    printf("Enter your gpa: ");
+    scanf("%f", &gpa);
+
+    printf("Enter your grade: ");
+    scanf(" %c", &grade);
+
+    getchar(); // this clears the buffer
+    printf("Enter your full name: ");  
+    //scanf("%s", &name); // this only works with 1 word; scanf stops when it sees white space
+    fgets(name, sizeof(name), stdin);
+    //name[strlen(name) - 1] = '\0';  // only use this if you call your name first. Eliminates \n after hitting enter "removes unwanted new line"
+
+    printf("Your name is %s\n", name);
+    printf("Your age is %d\n", age);
+    printf("Your gpa is %.1f\n", gpa);
+    printf("Your grade is %c\n", grade);
+    //printf("Your name is %s\n", name);
+
     return 0;
 }
